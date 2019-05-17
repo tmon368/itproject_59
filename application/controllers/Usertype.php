403495bc1dce9794bd17e106ca79bec071684f1f
charoenporn <tmon368@gmail.com>
2019-05-17 16:47:11 +0700

    new controller usertype

---
+++ application/controllers/Usertype.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Usertype extends CI_Controller {
+	public function index()
+	{
+		//List ข้อมูลมาแสดงในหน้าจอ
+	}
+	
+	public function new()
+	{
+	    //แสดงหน้าจอ form สำหรับบันทึกข้อมูลใหม่
+	}
+	
+	public function addnew()
+	{
+	    //รับข้อมูลจาก form  insert ลง DB
+	}
+	
+	public function edit()
+	{
+	    //แสดงหน้าจอ form สำแก้ไขข้อมูลใหม่
+	}
+	
+	public function update()
+	{
+	    //รับข้อมูลจาก form  update ลง DB
+	}
+}
