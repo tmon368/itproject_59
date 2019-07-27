ce17faff4971f2121415de8d65e87b94d61064b6
UNS_CT <UNS_CT@192.168.1.15>
2019-07-27 17:44:33 +0700

    หมวดแก้ใหม่

--- application/views/basicdata/offense/page_offense.php
+++ application/views/basicdata/offense/page_offense.php
@@ -45,7 +45,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h2 class="modal-title" id="exampleModalLongTitle">เพิ่มฐานความผิด2</h2>
+        <h2 class="modal-title" id="exampleModalLongTitle">เพิ่มฐานความผิด</h2>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">×</span>
         </button>
