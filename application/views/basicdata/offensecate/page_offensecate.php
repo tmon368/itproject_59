df2832dc0a3ea7256df1f6311f0ad77c4ce29d30
UNS_CT <UNS_CT@192.168.1.15>
2019-07-27 17:47:48 +0700

    หมวดแก้ไขใหม่

--- application/views/basicdata/offensecate/page_offensecate.php
+++ application/views/basicdata/offensecate/page_offensecate.php
@@ -18,7 +18,7 @@
     <div class="page-breadcrumb" id="nav_sty">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">จัดการข้อมูลพื้นฐาน</a></li>
+                <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">จัดการข้อมูลพื้นฐาน </a></li>
                 <li class="breadcrumb-item active" aria-current="page">หมวดความผิด</li>
             </ol>
         </nav>
