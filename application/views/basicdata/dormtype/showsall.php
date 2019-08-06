8bf147d9ee2f1bd713eb371a69a9d05faf06d31c
SunShine <SunShine@DESKTOP-4NR5C7K>
2019-08-06 14:39:31 +0700

    แก้ไขอีกครั้ง

--- application/views/basicdata/dormtype/showsall.php
+++ application/views/basicdata/dormtype/showsall.php
@@ -38,7 +38,7 @@
                 &nbsp;
             </div>
             <div id="myModal"  > </div>
-            <!-- Modal  เพิ่มข้อมูล -->
+            <!-- Modal การเพิ่มข้อมูล -->
             <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
                 aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
