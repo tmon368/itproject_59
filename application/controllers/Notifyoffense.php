a7934250582d4a19724beb36cd0b100f3387e310
BoomHumNoi <BoomHumNoi@10.112.2.203>
2019-08-21 02:50:28 -0700

    clf'sohkgg0h'gs96

--- application/controllers/Notifyoffense.php
+++ application/controllers/Notifyoffense.php
@@ -1,14 +1,103 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Reportstudent extends CI_Controller {
+class Notifyoffense extends CI_Controller {
+	function __construct(){
+		parent:: __construct();
+		$this->load->model('Notifyoffense_model', 'model');
+	}
 
 	public function index()
 	{
 		//List ข้อมูลมาแสดงในหน้าจอ
-		$this->load->view('template/template1');
-		
+	    $this->template();
+
+	}
+	
+	
+	public function template(){
+	    $this->load->view('template/template1');
+	    $this->load->view('template/template2');
+	    $this->load->view('menu/student/menu_student');
+	    $this->load->view('student/notify/notify_page');
+	    //$this->load->view('template/page_type_punish'); /*หน้าเพิ่มหมวดความผิด*/
+	    /*$this->load->view('template/page_usergroup');*/ /*หน้าเพิ่มประเภทผู้ใช้*/
+	    /*$this->load->view('template/page_import_data');*/
+	    $this->load->view('template/template5');
+	    $this->load->view('template/template6');
+	    
+	    
+	    
 	}
 
+	//ฟังก์ชันเรียกข้อมูลทั้งหมดจาก table personnel และแสดงข้อมูลในview
+	public function showAll(){
+		$result = $this->model->showAll();
+		echo json_encode($result);
+	}
 	
+	//ฟังก์ชันตรวจสอบ id ซ้ำกัน ตารางpersonnel
+	public function checkkey(){
+	    $result = $this->model->checkkey();
+	    if($result){
+	        $msg['success'] = true;
+	        
+	        
+	    }else{
+	        $msg['success'] = false;
+	        
+	    }
+	    echo json_encode($result);
+	}
+    
+	//ฟังก์ชันเพิ่มข้อมูล เมื่อเพิ่มข้อมูลเสร็จสิ้นจะแสดงข้อความ เพิ่มข้อมูลเรียบร้อย
+	public function addpersonnel(){
+		$result = $this->model->addpersonnel();
+		//$msg['success'] = false;
+		//$msg['type'] = 'add';
+		
+		
+		if($result){
+		    $msg['success'] = true;
+		    
+		    
+		}else{
+		    $msg['success'] = false;
+		    redirect(base_url() . 'index.php/personnel/index');
+		}
+		echo json_encode($msg);
+	}
+
+	//ฟังก์ชันแสดงการแก้ไขข้อมูล
+	public function editpersonnel(){
+	
+		$result = $this->model->editpersonnel();
+		echo json_encode($result);
+	}
+	
+	//ฟังก์ชันการอัพเดตข้อมูล เมื่ออัพเดตข้อมูลเสร็จสิ้นจะแสดงข้อความ แก้ไขข้อมูลเรียบร้อย
+	public function updatepersonnel(){
+		$result = $this->model->updatepersonnel();
+		$msg['success'] = false;
+		$msg['type'] = 'update';
+		if($result){
+		    $msg['success'] = true;
+		}else{
+		    $msg['success'] = false;
+		    redirect(base_url() . 'index.php/personnel/index');
+		}
+		echo json_encode($msg);
+	}
+    
+	//ฟังก์ชันการลบข้อมูล เมื่อลบข้อมูลเสร็จสิ้นจะแสดงข้อความ ลบข้อมูลเรียบร้อย
+	public function deletepersonnel(){
+		$result = $this->model->deletepersonnel();
+		if($result){
+		    $msg['success'] = true;     
+		}else{
+		    $msg['success'] = false;
+		    redirect(base_url() . 'index.php/personnel/index');
+		}
+		echo json_encode($msg);
+	}
 }
