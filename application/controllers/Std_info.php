26577dd5c4b8febffb2da9bebb736d5b57d9a63c
asus <asus@10.112.1.226>
2019-08-22 20:13:32 +0700

    ยานพาหนะ

--- application/controllers/Std_info.php
+++ application/controllers/Std_info.php
@@ -32,25 +32,12 @@ class Std_info extends CI_Controller {
         $result =  $this->model->selectstudent();
         echo json_encode($result);
     }
-    function selectpersonnel($row){
-        // $username = $this->session->userdata('username');
-        
-        $result = $this->model->selectstudent();
-        foreach ($result as $row){
-            $person_ID = $row->person_ID;
-            
-        }
-        $this->session->userdata('cur_ID');
-        //echo $person_ID;
-        $results = $this->model->selectpersonnel($person_ID);
-        
-        echo json_encode($results);
-    }
+  
     
     function selectvehicles(){
-        $username = $this->session->userdata('username');
+      //  $username = $this->session->userdata('username');
         
-        $result = $this->model->selectstudent($username);
+        $result = $this->model->selectvehicles();
         echo json_encode($result);
     }
     
