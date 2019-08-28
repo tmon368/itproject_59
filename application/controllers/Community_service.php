95e5646713956993509015075d8d1becdcb6bceb
asus <asus@192.168.1.15>
2019-08-28 21:17:32 +0700

    สร้าง controller และ model module บำเพ็ยประโยชน์

---
+++ application/controllers/Community_service.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class community_service extends CI_Controller {
+	function __construct(){
+		parent:: __construct();
+		$this->load->model('community_service_model', 'model');
+	}
+
+	public function index()
+	{
+		//List ข้อมูลมาแสดงในหน้าจอ
+	    $this->template();
+
+	}
+	
+	
+	public function template(){
+	    $this->load->view('template/template1');
+	    $this->load->view('template/template2');
+	    $this->load->view('template/template3');
+	    //$this->load->view('basicdata/place/page_place');
+	    //$this->load->view('template/page_type_punish'); /*หน้าเพิ่มหมวดความผิด*/
+	    /*$this->load->view('template/page_usergroup');*/ /*หน้าเพิ่มประเภทผู้ใช้*/
+	    /*$this->load->view('template/page_import_data');*/
+	    $this->load->view('template/template5');
+	    $this->load->view('template/template6');
+	    
+	    
+	    
+	}
+
+}
+
+
+
+
+
+
+
+
---
+++ application/models/community_service_model.php
@@ -0,0 +1,15 @@
+<?php
+
+class community_service_model extends CI_Model {
+    public function _construct()
+    {
+        parent::_construct();
+        
+    }
+    
+
+    
+    
+    
+    
+}
