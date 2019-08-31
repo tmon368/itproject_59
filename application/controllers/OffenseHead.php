a97b24a46b3acef8aab716600453ff248a720722
asus <asus@10.112.0.228>
2019-08-31 11:57:56 +0700

    ++

--- application/controllers/OffenseHead.php
+++ application/controllers/OffenseHead.php
@@ -1,7 +1,8 @@
 <?php
+require_once('Std_info.php');
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class OffenseHead extends CI_Controller {
+class OffenseHead extends Std_info {
     function __construct(){
         parent:: __construct();
         $this->load->model('OffenseHead_model', 'model');
@@ -9,6 +10,7 @@ class OffenseHead extends CI_Controller {
     public function index()
     {
         //List ข้อมูลมาแสดงในหน้าจอ
+        $this->logoutsession();
         $this->template();
         
     }
