b9c921f75a5766019cd68baafe57aa50634780ad
UNS_CT <UNS_CT@10.12.0.161>
2019-09-01 19:50:57 +0700

    ยังไม่เสร็จรอแก้

--- application/views/student/StudentPersonal/std_infopage.php
+++ application/views/student/StudentPersonal/std_infopage.php
@@ -42,7 +42,7 @@
   						รออนุมัติหลักฐานแย้ง : <input type="text"size="5"value=""style="border:0px; text-align:center;">ครั้ง<br><br>
   						รอการบำเพ็ญประโยชน์ :<input type="text"size="5"value=""style="border:0px; text-align:center;">ครั้ง<br><br>
   						รอการรับรองกิจกรรม : <input type="text"size="5"value=""style="border:0px; text-align:center;">ครั้ง<br><br>
-  						คืนคะแนนเรียบร้อยแล้ว :<input type="text"size="5"value=""style="border:0px; text-align:center;">ครั้ง<br><br> 
+  						
     					                    
   </table>
   </div>
@@ -58,106 +58,129 @@
  <div class="table-responsive">
  <table class="table table-hover m-b-0">
  <div class="breadcrumb" id="show_left">
-<br>
-<br>
-<br>
-<br>
-<body>
- <script >
- window.onload = function () {
-
-	 var chart = new CanvasJS.Chart("chartContainer", {
-	 	animationEnabled: true,
-	 	theme: "light2",
-	 	title:{
-	 		text: "Simple Line Chart"
-	 	},
-	 	axisY:{
-	 		includeZero: false
-	 	},
-	 	data: [{        
-	 		type: "line",       
-	 		dataPoints: [
-	 			{ y: 450 },
-	 			{ y: 414},
-	 			{ y: 520, indexLabel: "highest",markerColor: "red", markerType: "triangle" },
-	 			{ y: 460 },
-	 			{ y: 450 },
-	 			{ y: 500 },
-	 			{ y: 480 },
-	 			{ y: 480 },
-	 			{ y: 410 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
-	 			{ y: 500 },
-	 			{ y: 480 },
-	 			{ y: 510 }
-	 		]
-	 	}]
-	 });
-	 chart.render();
-
-	 }
-  </script>
-<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-<button class="btn invisible" id="backButton">< Back</button>
-<script src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
-<script  src="../re/js/canvasjs.min.js"></script>
-<script  src="../re/js/jquery.canvasjs.min.js"></script>
- </table></div>
- </body> 					
-</div>  
-</div>                      
-</div>
-</div>
-</div>
-</div>
-
-</body>
-
-<div class="col-sm-8">
- <div class="card comp-card">
-<div class="card-body">
-<div class="breadcrumb">
- <div class="table-responsive">
- <table class="table table-hover m-b-0">
- <div class="breadcrumb" id="show_right">
-<br>
-<br>
+กราฟแสดงคะแนนความประพฤตินักศึกษา
 <br>
 <br>
 <body>
  <script >
  window.onload = function () {
 
-	 var chart = new CanvasJS.Chart("chartContainer", {
-	 	animationEnabled: true,
-	 	theme: "light2",
-	 	title:{
-	 		text: "Simple Line Chart"
-	 	},
-	 	axisY:{
-	 		includeZero: false
-	 	},
-	 	data: [{        
-	 		type: "line",       
+	 var totalVisitors = 883000;
+	 var visitorsData = {
+	 	"New vs Returning Visitors": [{
+	 		click: visitorsChartDrilldownHandler,
+	 		cursor: "pointer",
+	 		explodeOnClick: false,
+	 		innerRadius: "75%",
+	 		legendMarkerType: "square",
+	 		name: "New vs Returning Visitors",
+	 		radius: "100%",
+	 		showInLegend: true,
+	 		startAngle: 90,
+	 		type: "doughnut",
 	 		dataPoints: [
-	 			{ y: 450 },
-	 			{ y: 414},
-	 			{ y: 520, indexLabel: "highest",markerColor: "red", markerType: "triangle" },
-	 			{ y: 460 },
-	 			{ y: 450 },
-	 			{ y: 500 },
-	 			{ y: 480 },
-	 			{ y: 480 },
-	 			{ y: 410 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
-	 			{ y: 500 },
-	 			{ y: 480 },
-	 			{ y: 510 }
+	 			{ y: 519960, name: "New Visitors", color: "#E7823A" },
+	 			{ y: 363040, name: "Returning Visitors", color: "#546BC1" }
+	 		]
+	 	}],
+	 	"New Visitors": [{
+	 		color: "#E7823A",
+	 		name: "New Visitors",
+	 		type: "column",
+	 		dataPoints: [
+	 	
+	 		]
+	 	}],
+	 	"Returning Visitors": [{
+	 		color: "#546BC1",
+	 		name: "Returning Visitors",
+	 		type: "column",
+	 		dataPoints: [
+	 
 	 		]
 	 	}]
-	 });
+	 };
+
+	 var newVSReturningVisitorsOptions = {
+	 	animationEnabled: true,
+	 	theme: "light2",
+	 	title: {
+	 		text: ""
+	 	},
+	 	subtitles: [{
+	 		text: "",
+	 		backgroundColor: "#2eacd1",
+	 		fontSize: 16,
+	 		fontColor: "white",
+	 		padding: 5
+	 	}],
+	 	legend: {
+	 		fontFamily: "calibri",
+	 		fontSize: 14,
+	 		itemTextFormatter: function (e) {
+	 			return e.dataPoint.name + ": " + Math.round(e.dataPoint.y / totalVisitors * 100) + "%";  
+	 		}
+	 	},
+	 	data: []
+	 };
+
+	 var visitorsDrilldownedChartOptions = {
+	 	animationEnabled: true,
+	 	theme: "light2",
+	 	axisX: {
+	 		labelFontColor: "#717171",
+	 		lineColor: "#a2a2a2",
+	 		tickColor: "#a2a2a2"
+	 	},
+	 	axisY: {
+	 		gridThickness: 0,
+	 		includeZero: false,
+	 		labelFontColor: "#717171",
+	 		lineColor: "#a2a2a2",
+	 		tickColor: "#a2a2a2",
+	 		lineThickness: 1
+	 	},
+	 	data: []
+	 };
+
+	 var chart = new CanvasJS.Chart("chartContainer", newVSReturningVisitorsOptions);
+	 chart.options.data = visitorsData["New vs Returning Visitors"];
 	 chart.render();
 
+	 function visitorsChartDrilldownHandler(e) {
+	 	chart = new CanvasJS.Chart("chartContainer", visitorsDrilldownedChartOptions);
+	 	chart.options.data = visitorsData[e.dataPoint.name];
+	 	chart.options.title = { text: e.dataPoint.name }
+	 	chart.render();
+	 	$("#backButton").toggleClass("invisible");
 	 }
+
+	 $("#backButton").click(function() { 
+	 	$(this).toggleClass("invisible");
+	 	chart = new CanvasJS.Chart("chartContainer", newVSReturningVisitorsOptions);
+	 	chart.options.data = visitorsData["New vs Returning Visitors"];
+	 	chart.render();
+	 });
+
+	 }
+	 </script>
+	 <style>
+	   #backButton {
+	 	border-radius: 4px;
+	 	padding: 8px;
+	 	border: none;
+	 	font-size: 16px;
+	 	background-color: #2eacd1;
+	 	color: white;
+	 	position: absolute;
+	 	top: 10px;
+	 	right: 10px;
+	 	cursor: pointer;
+	   }
+	   .invisible {
+	     display: none;
+	   }
+	 </style>
   </script>
 <div id="chartContainer" style="height: 370px; width: 100%;"></div>
 <button class="btn invisible" id="backButton">< Back</button>
@@ -177,7 +200,7 @@
 </html>
 <script>
     $(document).ready(function() {
-    	showAll();
+    	selectdetailfirstpage();
    
     	 
         //$('[data-toggle="popover"]').popover();
@@ -188,29 +211,26 @@
     
 
     
-  function showAll() {
+  function selectdetailfirstpage() {
               $.ajax({
                     type: 'ajax',
-                    url: '<?php echo base_url() ?>index.php/Student_dashboard/selectstudentfirstpage',
+                    url: '<?php echo base_url() ?>index.php/Student_dashboard/selectdetailfirstpage',
                     async: false,
                     dataType: 'json',
                     success: function() {
 
-                    	//$("#S_ID").html(data[0].S_ID);
-                    	//$("#std_fname").html(data[0].std_fname);
-                    	//$("#std_lname").html(data[0].std_lname);
-                    	//$("#cur_name").html(data[0].cur_name);
-                    	//$("#dept_name").html(data[0].dept_name);
-                    	//$("#status_name").html(data[0].status_name);
+                    	$("#num_off").html(data[0].num_off);
+                    	$("#status_off").html(data[0].status_off);
+                   
                     
 
                     	
                     	
                     	
-                    	//$("#email1").html(data[0].email);    	
+                    	$("#email1").html(data[0].email);    	
                     },
                     error: function() {
-                        //alert('ไม่มีข้อมูล'); 
+                       // alert('ไม่มีข้อมูล'); 
                     }
                 });
                 
