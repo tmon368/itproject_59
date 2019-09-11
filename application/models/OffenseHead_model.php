b2e55cbbee278e595274191998a85db8dc0fd410
asus <asus@192.168.1.11>
2019-09-11 14:47:20 +0700

    ++ ต๋อง

--- application/models/OffenseHead_model.php
+++ application/models/OffenseHead_model.php
@@ -10,12 +10,12 @@ class OffenseHead_model extends CI_Model {
     public function selectstudentoffensehead(){
         $student = $this->session->userdata('student');
         $this->db->select('*');
-        $this->db->from('offensehead oh');
-        $this->db->join('student s', 'oh.S_ID=s.S_ID');
+        $this->db->from('offensestd ostd');
+        $this->db->join('offensehead oh', 'ostd.oh_ID=oh.oh_ID');
         $this->db->join('offense o', 'oh.off_ID=o.off_ID'); 
         $this->db->join('place p', 'oh.place_ID=p.place_ID');
-       // $this->db->join('offevidence ov', 'oh.offeviden_ID=ov.offeviden_ID');
-        $this->db->where('oh.S_ID',$student);
+        $this->db->join('offevidence ov', 'oh.oh_ID=ov.oh_ID');
+        $this->db->where('ostd.S_ID',$student);
         $query = $this->db->get();
         $student = array();
         $student = $query->result_array();
