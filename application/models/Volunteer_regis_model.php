8952ef8719e0828588f32b779eda2d586b6b7121
Boomsamui2499 <tossawat.ma@mail.wu.ac.th>
2019-09-18 21:06:40 +0700

    จัดไปแบง

--- application/models/Volunteer_regis_model.php
+++ application/models/Volunteer_regis_model.php
@@ -15,10 +15,9 @@ class Volunteer_regis_model extends CI_Model {
         //$service_ID= $this->input->post('txtdelID');
        //$student=59111111;
         //echo $person_ID;
-    //$student = $this->session->userdata('student');
+        //$student = $this->session->userdata('student');
         $this->db->select('*');
-        $this->db->from('student s');
-       $this->db->join('service sv', 's.S_ID=sv.S_ID');
+        $this->db->from('Service sv');
         $this->db->join('personnel p', 'sv.person_ID=p.person_ID');
         //$this->db->where('s.S_ID', $student);
         $query = $this->db->get();
