cbd0d4196295a56a94f2d48728b40f93fbb34afd
Thevenombom <Thevenombom@172.16.93.18>
2019-09-19 22:23:47 +0700

    auto complect

--- application/views/student/VolunteerAc/view_VolunteerAc.php
+++ application/views/student/VolunteerAc/view_VolunteerAc.php
@@ -34,122 +34,7 @@
         }
     </style>
 
-    <script>
-        var off_per = 1;
-
-        function student_change(id) {
-            $(".loader").show();
-            $('#std_2').typeahead({
-
-                source: ["", "C++"]
-                //onkeypress="student_change(this)"
-            });
-
-        }
-
-        function click_btnre(id) {
-            //alert(id.value);
-            $('#student' + id).html('');
-
-        }
-
-
-        //clear value on text
-        function clear_value() {
-            $('#service_ID').val("");
-            $('#service_name').val("");
-            $('#person_ID').val("");
-            $('#S_ID').val("");
-            $('#place').val("");
-            $('#service_date').val("");
-            $('#service_time').val("");
-            $('#service_hour').val("");
-            $('#status').val("");
-            $('#approval_date	').val("");
-            $('#offer_status').val("");
-       
-        }
-
-        //Search student data in form
-        function Search_data(id) {
-
-            var idstd = id.value;
-            alert("กำลังค้นหา");
-            clear_value(); //clear value on textbox
-            //alert(idstd);
-
-
-            if (idstd) {
-                //alert ("GGG");
-
-                $.ajax({
-                    type: 'POST',
-                    url: '<?php echo site_url("VolunteerAC/selectapersennel") ?>',
-                    data: 'S_ID=' + idstd,
-                    dataType: 'json',
-                    success: function(data) {
-                        //alert("Having Data...");
-
-                        $.each(data, function(key, value) {
-
-                            var temp = value;
-
-                            if (key == ) {
-                                //alert("Array student");
-                                $('#person_fname').val(value.person_fname);
-                                $('#person_lname').val(value.person_lname);
-                               
-
-
-                              
-
-                            } else if (key == "verhicles") {
-                                //alert("SEE verhicles");
-                                var verhicle = value;
-
-                                $.each(verhicle, function(key, value) {
-                                    //alert(key);
-
-
-                                    //check type vehicle
-                                    if (value.vetype_ID == 1) {
-                                        //alert ("Motorcycles TYPE");
-                                        $('#regis_num').val(value.regist_num);
-                                        $('#province_bic').val(value.province);
-                                    } else if (value.vetype_ID == 2) {
-                                        //alert("CAR TYPE");
-                                        $('#regis_car').val(value.regist_num);
-                                        $('#provin_car').val(value.province);
-
-                                    } else {
-
-                                    }
-                                    //alert(value.regist_num);
-                                });
-
-                            }
-
-                            //console.log(data.verhicles);
-
-                        });
-
-                        //console.log(data);
-
-                    }
-
-                });
-            } else {
-                alert("ไม่มีข้อมูล");
-            }
-
-
-
-
-
-
-
-        }
-    </script>
+   
 
 </head>
 
@@ -199,12 +84,7 @@
                                 <!--  CONTENT -->
 
                                 <form action="">
-                                    <div class="row">
-                                        <div class="col-sm-6"> </div>
-                                        <div class="col-sm-6 padding_b">
-                                            <div class="form-inline"><label for="">รหัสกิจกรรม:</label><input type="text" name="service_ID" id="service_ID" class="form-control style_input" ></div>
-                                        </div>
-                                    </div>
+                                  
                                     
                                     <div class="row">
                                         <div class="col-sm-8">
@@ -244,7 +124,7 @@
                                         <div class="col-sm-8"> </div>
                                         <div class="col-sm-6 padding_b">
                                             <div class="form-inline"><label for="">จำนวนผู้เข้าร่วม</label><font color="red">* </font>:&nbsp;&nbsp;&nbsp;&nbsp;
-                                          &nbsp;&nbsp;  <input type="text" name="service_hour" id="service_hour" class=" style_input"  >
+                                          &nbsp;&nbsp;  <input type="text" name="service_hour" id="service_hour" class=" style_input" style="width:50px;" >
                                             </div>
                                         </div>
                                     </div>
@@ -256,16 +136,15 @@
                                             </div>
                                         </div>
                                     </div>
-                    <div class="row">
-                      <div class="col-sm-12">
-                        <label for="">ผู้รับรองกิจกรรม:</label><font color="red">* </font>:&nbsp;&nbsp;&nbsp;
-                         <a href="javascript:;" id="add">
-                         <span class="badge badge-pill badge-primary"> + เพิ่มผู้รับรองกิจกรรม</span></a>
-                       </div>
-                             </div>
-                            
-                                        <div class="add_person">
+                                      <div class="row">
+                                        <div class="col-sm-8"> </div>
+                                        <div class="col-sm-6 padding_b">
+                                            <div class="form-inline"><label for="">ผู้รับรองกิจกรรม</label><font color="red">* </font>:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                         <input type="text" name="add_persennel" id="add_persennel" class=" style_input" style="width:200px;" >
+                                          <input type="hidden" name="fullname" id="fullname">
+                                            </div>
                                         </div>
+                                    </div>
                                     
                                  
                                  
@@ -476,26 +355,46 @@
 
 
         }
-        /* else {
-                            $('#state').html('<option value="">Select country first</option>');
-                            $('#city').html('<option value="">Select state first</option>');*/
+        
 
     });
 
 
 
-    //});
+    $('#add_persennel').typeahead({
+
+        source: function(query, result) {
+            $.ajax({
+                url: "<?php echo site_url('VolunteerAC/selectapersennel') ?>",
+                method: "POST",
+                data: {
+                    query: query
+                },
+                dataType: "json",
+                success: function(data) {
+                    
+                    result($.map(data, function(item) {
+                    
+                        $('#fullname').val(item.fullname);
+                        return item.fullname; 
+                    }));
+                }
+            })
+        }
+
+
+    });
+
 
 
 
 
     $('#btnAdd').click(function() {
-        var date = new Date();
-        //date_off = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
-        date_off = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-        $('#exampleModalCenter').modal('show');
-        $('#notifica_date').val(date_off); //set of date in form
-        $('#evidenre_date').val(date_off); //set of date in form
+     
+        
+       
+   $('#exampleModalCenter').modal('show');
+       
 
     });
 
@@ -531,29 +430,7 @@
 
 
 
-    $('#add').click(function() {
-        var html = '';
-
-        html += '<div id="student' + off_per + '">';
-
-        html += '<div class="row">';
-        
-        html += '<div class="col-sm-4"> <label for="">ชื่อ:</label> <input type="text" name="person_fname" id="person_fname" >   </div>';
-        html += '<div class="col-sm-4"> <label for="">นามสกุล:</label> <input type="text" name="	person_lname" id="person_lname" >  </div>';
-        html += '</div>';
-
-        
-        html += '<div class="row">';
-        html += '<div class="col-sm-12" style="text-align: right;"> <a href="javascript:;" id="" onclick="Search_data(person_fname)"><span class="fa fa-search"></span></a> <a href="javascript:;" id="" onclick="click_btnre(' + off_per + ')"><span class="fa fa-trash" style="font-size: 1.5rem;"></span>  </div>'; //<a href="javascript:;" id="Seachdata"><span class="fa fa-search"></span></a>
-        html += '</div>';
-        //<button type="button" name="remove" id="' + off_per + '" class="btn btn-danger btn_remove" onclick="click_btnre(' + off_per + ')">X</button>
-
-        html += '</div>';
-
-
-        off_per++;
-        $('.add_person').append(html);
-    });
+    
 </script>
 
 
