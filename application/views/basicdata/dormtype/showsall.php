1f76735c140938fd5c0e42beb40f43d9e625ab28
SunShine <SunShine@DESKTOP-4NR5C7K>
2019-10-07 17:22:43 +0700

    " หาย

--- application/views/basicdata/dormtype/showsall.php
+++ application/views/basicdata/dormtype/showsall.php
@@ -39,7 +39,7 @@
            
             <div class="card-body" id="card_1">
                 <button type="button" id="btnAdd" class="btn btn-inverse-primary btn-fw" data-toggle="modal">
-                    <span><i class="fas fa-plus" id="btnAdd></i></span>เพิ่มประเภทหอพัก
+                    <span><i class="fas fa-plus" id="btnAdd"></i></span>เพิ่มประเภทหอพัก
                 </button>
                 &nbsp;
             </div>
