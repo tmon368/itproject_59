8abd8cd49a55946257ab1c2455b4846aa1a97017
Guppiya2010 <Guppiya2010@hotmail.com>
2019-10-28 10:04:28 +0700

    ++ แก้หน้าข้อมูลส่วนตัว logout ไม่ได้

--- application/views/student/StudentPersonal/std_personal_Information.php
+++ application/views/student/StudentPersonal/std_personal_Information.php
@@ -1,10 +1,10 @@
 <!doctype html>
 <html lang="en">
 <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script> -->
 
 <center>
     <strong>
