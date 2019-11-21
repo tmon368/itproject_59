6762a9f63c6413f9781bcdb7503825943f84909e
boomsamui2499 <tossawat.ma@mail.wu.ac.th>
2019-11-21 23:31:34 +0700

    เตย

--- application/models/dormtype_model.php
+++ application/models/dormtype_model.php
@@ -9,13 +9,12 @@ class dormtype_model extends CI_Model {
     
     //ฟังก์ชันแสดงข้อมูลทั้งหมด จาก table dormtype โดยเรียงลำดับจาก dormtype_ID
  public function showAll(){
-     $flag=1;
+     $flag=0;
      
      $this->db->select('*');
-     $this->db->from('Dormitory c'); // หมวดความผิด // หอพัก
-     $this->db->join('DormType d', 'c.dormtype_ID=d.dormtype_ID'); // ฐานความผิด  // ประเภทหอพัก
+     $this->db->from('DormType'); // หมวดความผิด // หอพัก
      //$this->db->order_by('off_ID', 'ASC');
-     $this->db->where('flagg', $flag);
+     $this->db->where('flag', $flag);
      $query = $this->db->get();
      if($query->num_rows() > 0){
          return $query->result();
