9fee394d862ca492329a5711955cf788da2eda5b
Guppiya2010 <Guppiya2010@hotmail.com>
2019-12-01 13:27:53 +0700

    ++

--- application/models/Discipline_officer_dashboard_model.php
+++ application/models/Discipline_officer_dashboard_model.php
@@ -60,8 +60,8 @@ class Discipline_officer_dashboard_model extends CI_Model {
        $query= $this->db->query("SELECT divisions.dept_ID, dept_name as label,COUNT(offensestd.S_ID) as y ,offensecate.oc_ID ,offensecate.oc_desc FROM `divisions` ,`offensestd` ,`offensehead`, `offense`,`offensecate`,`student`,`curriculum` WHERE offensecate.oc_ID='+$oc_ID+' and offensestd.oh_ID=offensehead.oh_ID and offensehead.off_ID=offense.off_ID and offense.oc_ID=offensecate.oc_ID and offensestd.S_ID=student.S_ID and student.cur_ID=curriculum.cur_ID and curriculum.dept_ID=divisions.dept_ID  GROUP BY divisions.dept_ID");
        $data = array();
         $data = $query->result_array();
-        var_dump( $data);
-       die();
+       // var_dump( $data);
+       //die();
        if($data !=NULL){
         return $data;
     }else{
