c5221334ed2f6e16a9b3d88eed41f7a8922130b4
Guppiya2010 <Guppiya2010@hotmail.com>
2019-12-03 14:15:22 +0700

    API วินัย(join)

--- application/models/Discipline_officer_dashboard_model.php
+++ application/models/Discipline_officer_dashboard_model.php
@@ -10,26 +10,16 @@ class Discipline_officer_dashboard_model extends CI_Model {
     public function getDashboard(){
         //SELECT offensecate.*,COUNT(offensestd.S_ID) as std FROM `offensecate`, `offensestd` , `offensehead` , `offense`   WHERE offensestd.oh_ID=offensehead.oh_ID and offensehead.off_ID=offense.off_ID and offense.oc_ID=offensecate.oc_ID  GROUP BY offensecate.oc_ID
 
-        //SELECT offensecate.*,COUNT(offensestd.S_ID) as std FROM `offensecate`, `offensestd` , `offensehead` , `offense`   WHERE 
-        //offensestd.oh_ID=offensehead.oh_ID 
-        //and offensehead.off_ID=offense.off_ID 
-        //and offense.oc_ID=offensecate.oc_ID  GROUP BY offensecate.oc_ID
-        $query= $this->db->query('SELECT offensecate.oc_ID,offensecate.oc_desc as label,COUNT(offensestd.S_ID) as y FROM `offensecate`, `offensestd` , `offensehead` , `offense`   WHERE offensestd.oh_ID=offensehead.oh_ID and offensehead.off_ID=offense.off_ID and offense.oc_ID=offensecate.oc_ID  GROUP BY offensecate.oc_ID');
-        // $this->db->select('offensecate.* as label,count(offensestd.S_ID) as y');
-        // $this->db->group_by('offensecate.oc_ID');
-        // $this->db->from('offensecate oc');
-        // $this->db->join('offense o','oc.oc_ID=o.oc_ID');
-        // $this->db->join('offensehead oh','o.off_ID=oh.off_ID'); 
-        // $this->db->join('offensestd ostd','น้.oh_ID =ostd.oh_ID');
-         
-        
-        //$this->db->where('p.username',$teacher);
-        // $query = $this->db->get();
-        // $student = array();
-        // $student = $query->result_array();
+        //$query= $this->db->query('SELECT offensecate.oc_ID,offensecate.oc_desc as label,COUNT(offensestd.S_ID) as y FROM `offensecate`, `offensestd` , `offensehead` , `offense`   WHERE offensestd.oh_ID=offensehead.oh_ID and offensehead.off_ID=offense.off_ID and offense.oc_ID=offensecate.oc_ID  GROUP BY offensecate.oc_ID');
+        $this->db->select('oc.oc_ID,oc.oc_desc as label,count(ostd.S_ID) as y');   
+        $this->db->from('offensestd ostd');
+        $this->db->join('offensehead oh','ostd.oh_ID=oh.oh_ID');
+        $this->db->join('offense o','oh.off_ID=o.off_ID'); 
+        $this->db->join('offensecate oc','o.oc_ID=oc.oc_ID');
+        $this->db->group_by('oc.oc_ID');
+        $query = $this->db->get();
         $data = array();
         $data = $query->result_array();
-        // var_dump($data);
         // echo "<br><br><br>";
         $calnumstd =0;
         foreach($data as $value){
@@ -39,8 +29,8 @@ class Discipline_officer_dashboard_model extends CI_Model {
 
         }
        $data['numstd'] = $calnumstd;
-        // var_dump($data);
-        // die();
+         //var_dump($data);
+         //die();
         
         
         
@@ -55,13 +45,26 @@ class Discipline_officer_dashboard_model extends CI_Model {
     public function getDashboardAll(){
         $oc_ID = $_GET['oc_ID'];
         //echo $oc_ID;
-        //$oc_ID = 6;
-       // SELECT divisions.dept_ID, dept_name,COUNT(offensestd.S_ID) as y FROM `divisions` ,`offensestd` ,`offensehead`, `offense`,`offensecate`,`student`,`curriculum` WHERE offensecate.oc_ID='11' and offensestd.oh_ID=offensehead.oh_ID and offensehead.off_ID=offense.off_ID and offense.oc_ID=offensecate.oc_ID and offensestd.S_ID=student.S_ID and student.cur_ID=curriculum.cur_ID and curriculum.dept_ID=divisions.dept_ID GROUP BY divisions.dept_ID
-       $query= $this->db->query("SELECT divisions.dept_ID, dept_name as label,COUNT(offensestd.S_ID) as y ,offensecate.oc_ID ,offensecate.oc_desc FROM `divisions` ,`offensestd` ,`offensehead`, `offense`,`offensecate`,`student`,`curriculum` WHERE offensecate.oc_ID='+$oc_ID+' and offensestd.oh_ID=offensehead.oh_ID and offensehead.off_ID=offense.off_ID and offense.oc_ID=offensecate.oc_ID and offensestd.S_ID=student.S_ID and student.cur_ID=curriculum.cur_ID and curriculum.dept_ID=divisions.dept_ID  GROUP BY divisions.dept_ID");
+        //$oc_ID = 8;
+       
+       //$query= $this->db->query("SELECT divisions.dept_ID, dept_name as label,COUNT(offensestd.S_ID) as y ,offensecate.oc_ID ,offensecate.oc_desc FROM `divisions` ,`offensestd` ,`offensehead`, `offense`,`offensecate`,`student`,`curriculum` WHERE offensecate.oc_ID='+$oc_ID+' and offensestd.oh_ID=offensehead.oh_ID and offensehead.off_ID=offense.off_ID and offense.oc_ID=offensecate.oc_ID and offensestd.S_ID=student.S_ID and student.cur_ID=curriculum.cur_ID and curriculum.dept_ID=divisions.dept_ID  GROUP BY divisions.dept_ID");
+       
+       $this->db->select('d.dept_ID, d.dept_name as label,COUNT(ostd.S_ID) as y ,oc.oc_ID ,oc.oc_desc');   
+       $this->db->from('offensestd ostd');
+       $this->db->join('offensehead oh','ostd.oh_ID=oh.oh_ID');
+       $this->db->join('offense o','oh.off_ID=o.off_ID'); 
+        $this->db->join('offensecate oc','o.oc_ID=oc.oc_ID');
+        $this->db->join('student std','ostd.S_ID=std.S_ID');
+        $this->db->join('curriculum c','std.cur_ID=c.cur_ID');
+        $this->db->join('divisions d','c.dept_ID=d.dept_ID');    
+       $this->db->group_by('d.dept_ID');
+       $this->db->where('oc.oc_ID',$oc_ID);
+
+        $query = $this->db->get();
        $data = array();
         $data = $query->result_array();
-       // var_dump( $data);
-       //die();
+    //    var_dump($data);
+    //    die();
        if($data !=NULL){
         return $data;
     }else{
@@ -73,24 +76,34 @@ class Discipline_officer_dashboard_model extends CI_Model {
     
    
     public function getGraphDataSchool(){
-        //SELECT curriculum.cur_ID, curriculum.cur_name as label,curriculum.dept_ID, COUNT(offensestd.S_ID) as y  FROM `offensestd` ,`offensehead`, `offense`,`offensecate`,`student`,`curriculum`,`divisions` WHERE divisions.dept_ID = '22' and offensecate.oc_ID='8'  and offensestd.oh_ID=offensehead.oh_ID and offensehead.off_ID=offense.off_ID and offense.oc_ID=offensecate.oc_ID and offensestd.S_ID=student.S_ID and student.cur_ID=curriculum.cur_ID and curriculum.dept_ID=divisions.dept_ID   GROUP BY curriculum.cur_ID
         $oc_ID = 8;
         $dept_ID = 22;
-        // $field = array(
-        //     'oc_ID'=>8,
-        //     'dept_ID'=>22
-            
-        //     );
         // $field = array(
         //     'oc_ID'=>$this->input->post('oc_ID'),
         //     'dept_ID'=>$this->input->post('dept_ID')
             
         //     );
-         $query= $this->db->query("SELECT curriculum.cur_ID, curriculum.cur_name as label,curriculum.dept_ID, COUNT(offensestd.S_ID) as y  FROM `offensestd` ,`offensehead`, `offense`,`offensecate`,`student`,`curriculum`,`divisions` WHERE divisions.dept_ID = '+$dept_ID+' and offensecate.oc_ID='+$oc_ID+'  and offensestd.oh_ID=offensehead.oh_ID and offensehead.off_ID=offense.off_ID and offense.oc_ID=offensecate.oc_ID and offensestd.S_ID=student.S_ID and student.cur_ID=curriculum.cur_ID and curriculum.dept_ID=divisions.dept_ID   GROUP BY curriculum.cur_ID");
+        // $query= $this->db->query("SELECT curriculum.cur_ID, curriculum.cur_name as label,curriculum.dept_ID, COUNT(offensestd.S_ID) as y  FROM `offensestd` ,`offensehead`, `offense`,`offensecate`,`student`,`curriculum`,`divisions` WHERE divisions.dept_ID = '+$dept_ID+' and offensecate.oc_ID='+$oc_ID+'  and offensestd.oh_ID=offensehead.oh_ID and offensehead.off_ID=offense.off_ID and offense.oc_ID=offensecate.oc_ID and offensestd.S_ID=student.S_ID and student.cur_ID=curriculum.cur_ID and curriculum.dept_ID=divisions.dept_ID   GROUP BY curriculum.cur_ID");
+         
+
+         $this->db->select('c.cur_ID, c.cur_name as label,c.dept_ID, COUNT(ostd.S_ID) as y');   
+         $this->db->from('offensestd ostd');
+         $this->db->join('offensehead oh','ostd.oh_ID=oh.oh_ID');
+         $this->db->join('offense o','oh.off_ID=o.off_ID'); 
+          $this->db->join('offensecate oc','o.oc_ID=oc.oc_ID');
+          $this->db->join('student std','ostd.S_ID=std.S_ID');
+          $this->db->join('curriculum c','std.cur_ID=c.cur_ID');
+          $this->db->join('divisions d','c.dept_ID=d.dept_ID'); 
+         $this->db->group_by('c.cur_ID');
+         $this->db->where('d.dept_ID',$dept_ID);
+         $this->db->where('oc.oc_ID',$oc_ID);
+         
+         $query = $this->db->get();
          $data = array();
          $data = $query->result_array();
-      // var_dump( $data);
-       //die();
+       //var_dump( $data);
+      // die();
+      
        if($data !=NULL){
         return $data;
     }else{
