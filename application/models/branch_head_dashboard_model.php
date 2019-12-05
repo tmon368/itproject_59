e6ca96abf55002feba3ea72c00111b3c1b914b61
Boomsamui2499 <tossawat.ma@mail.wu.ac.th>
2019-12-05 13:30:18 +0700

    api 4.2 verygood

--- application/models/branch_head_dashboard_model.php
+++ application/models/branch_head_dashboard_model.php
@@ -50,28 +50,31 @@ class branch_head_dashboard_model extends CI_Model {
     
    
 public function getAllSTD(){
-    // $cur_ID = $this->session->userdata('cur_ID');
+    $perid=$this->session->userdata('username');
 
-$student = 7054545;
+// $perid = 7054545;
 //SELECT DISTINCT offensestd.S_ID,student.std_fname,student.std_lname,student.behavior_score FROM offensestd,student WHERE offensestd.S_ID=student.S_ID
 //  $cur_ID = $_GET['cur_ID'];
-$cur_ID = 21;
+// $cur_ID = 18;
 
 
 // $this->db->distinct();
-$this->db->select('ostd.S_ID,s.std_fname,s.std_lname,s.behavior_score');
+$this->db->select('ostd.S_ID,s.std_fname,s.std_lname,s.behavior_score,c.cur_ID');
 //$this->db->select('*');
 $this->db->from('offensestd ostd');
 $this->db->join('offensehead o','ostd.oh_ID=o.oh_ID');
 $this->db->join('offense of','o.off_ID=of.off_ID');
 $this->db->join('offensecate oc','of.oc_ID=oc.oc_ID');
 $this->db->join('student s','ostd.S_ID=s.S_ID');
+// $this->db->join('personnel p','s.person_ID=p.person_ID');
 $this->db->join('curriculum c','s.cur_ID=c.cur_ID');
-$this->db->join('personnel p','c.cur_ID=c.cur_ID');
+$this->db->join('personnel p','c.cur_ID=p.cur_ID');
 
-// $this->db->group_by('c.cur_ID');
-$this->db->where('c.cur_ID',$cur_ID);
-$this->db->where('p.person_ID',$student);
+// $this->db->group_by('p.cur_ID');
+// $this->db->where('p.cur_ID=p.cur_ID');
+$this->db->where('p.person_ID',$perid);
+// $this->db->where('p.cur_ID=p.cur_ID');
+
 
 
 
