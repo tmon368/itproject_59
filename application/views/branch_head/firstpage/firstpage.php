5ba1c4daf191053489aab3499ada6f7e233b21e4
UNS_CT <UNS_CT@DESKTOP-2G7U98O>
2019-12-07 14:27:35 +0700

    brandhead.1

--- application/views/branch_head/firstpage/firstpage.php
+++ application/views/branch_head/firstpage/firstpage.php
@@ -1,1 +1,91 @@
-Hii   branch_head(หัวหน้างานสาขา)  ครับผม
+<!doctype html>
+<html lang="en">
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+
+<center>
+    <strong><div class="alert alert-success" role="alert" style="display: none;"></div></strong>
+    <strong><div class="alert alert-danger" role="alert" style="display: none;"></div></strong>
+    <strong><div class="alert alert-warning" role="alert" style="display: none;"></div></strong>
+</center>
+
+<head>
+    <title></title>   
+</head>
+
+<body>
+    <meta charset="UTF-8">
+ <div class="col-lg-12 grid-margin stretch-card">
+ 
+  <div class="col-lg-3 grid-margin stretch-card"> 
+         
+<div class="col-lg-12 ">
+
+      <div class="col-lg-12 ">
+            <div class="card shadow mb-3">
+          <div class="card-header" id="card_2"></div>              
+        <div class="card-body " id="card_1"> 
+        <br><br><br><br><br>
+        
+        </div> 
+</div>
+</div>    
+
+<div class="col-lg-12 ">
+            <div class="card shadow mb-3">
+          <div class="card-header" id="card_2"></div>              
+        <div class="card-body " id="card_1"> 
+        <br><br>
+        </div> 
+</div>
+</div>
+
+<div class="col-lg-12 ">
+            <div class="card shadow mb-3">
+          <div class="card-header" id="card_2"></div>                    
+        <div class="card-body " id="card_1"> 
+        <br><br><br><br><br><br><br><br><br><br>
+        </div> 
+</div>
+</div>
+
+
+
+</div>
+
+
+
+
+</div>
+
+
+<div class="col-lg-9 grid-margin stretch-card">
+<div class="col-lg-12 ">
+            <div class="card shadow mb-3">
+          <div class="card-header" id="card_2">
+                    <h6 class="m-0 text-primary"><span  class=""></span>&nbsp;</h6>
+                </div>   
+                     	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                     	
+        <div class="card-body " id="card_1"> </div> 
+</div>
+
+
+<div class="col-lg-9 ">
+            <div class="card shadow mb-3">
+          <div class="card-header" id="card_2"></div>              
+        <div class="card-body " id="card_1"> 
+        <br><br><br><br><br><br><br><br>
+        </div> 
+</div>
+</div>
+
+</div>
+
+
+
+
+</div>
+
+</div>
+</body>
+</html>
