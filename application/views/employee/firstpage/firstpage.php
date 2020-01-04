8e4dd4f5d01beb167a7377abbb160c626ce32703
Suppakrit422 <suppakrit40@hotmail.com>
2020-01-04 20:15:32 +0700

    editfile

--- application/views/employee/firstpage/firstpage.php
+++ application/views/employee/firstpage/firstpage.php
@@ -1,1 +1,8 @@
-Hii employee(พนักงาน) ครับผม
+<link rel="stylesheet" href="<?php echo base_url('re/css/css_user_security.css'); ?>">
+
+<head>
+    <title>หน้าแรก | ระบบวินัยนักศึกษามหาวิทยาลัยวลัยลักษณ์</title>
+    <style>
+
+    </style>
+</head>
