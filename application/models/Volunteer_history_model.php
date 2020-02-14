07d721d3c932f967f7ef311f2651b6d1c3f12f64
Guppiya2010 <Guppiya2010@hotmail.com>
2020-02-15 01:11:18 +0700

    ลองดูแบงค์

--- application/models/Volunteer_history_model.php
+++ application/models/Volunteer_history_model.php
@@ -10,19 +10,20 @@ class Volunteer_history_model extends CI_Model {
     function showAll(){
         $i=0;
        $student = $this->session->userdata('student');
-       // $student =59123456;
+        //$student =59123456;
 
         $this->db->select('*');
         $this->db->from('service sv');
         $this->db->join('personnel p', 'sv.person_ID=p.person_ID');
         $this->db->join('participationactivities pp', 'sv.service_ID=pp.service_ID');
         $this->db->where('pp.S_ID', $student);
-        $this->db->where('pp.results', '0');
+        //$this->db->where('pp.results', '0');
         //var_dump($query->result());
         //$this->db->where('service_ID', $id);
         $query = $this->db->get();
         $showall = array();
         $showall = $query->result_array();
+         
         foreach($showall as $value){
             $data = $value['status'];
             $start_time = date_format(date_create($value['start_time']),"H:i");
@@ -35,8 +36,8 @@ class Volunteer_history_model extends CI_Model {
         $showall[$i]["end_time"] = $end_time;
         $i++;
     }
-        //var_dump($showall);   
-       // die();     
+    // var_dump($showall);   
+    // die();
         if($showall > 0){
             return $showall;
         }else{
