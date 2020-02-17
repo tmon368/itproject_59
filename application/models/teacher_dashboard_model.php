bea883d2d349984a8405859293d63c3cd0b2b1b7
Guppiya2010 <Guppiya2010@hotmail.com>
2020-02-17 17:50:24 +0700

    เพิ่มชื่อหอพัก

--- application/models/teacher_dashboard_model.php
+++ application/models/teacher_dashboard_model.php
@@ -116,10 +116,11 @@ class teacher_dashboard_model extends CI_Model {
     function studentinactivity(){
         $id = $this->input->get('id');
         //$id = 3;
-        $this->db->select('s.S_ID,s.std_fname,s.std_lname,s.email,s.phone');
+        $this->db->select('s.S_ID,s.std_fname,s.std_lname,s.email,s.phone,d.dname');
         $this->db->from('service sv');
         $this->db->join('participationactivities p','sv.service_ID=p.service_ID');
         $this->db->join('student s','p.S_ID=s.S_ID');
+        $this->db->join('dormitory d','s.dorm_ID=d.dorm_ID');
         $this->db->where('sv.service_ID',$id);
         $query = $this->db->get();
         $stdacticity = array();
