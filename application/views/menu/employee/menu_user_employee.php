79089a252ae1a07f0be76a620b074bc23525d80b
Thevenombom <venombom59114421@gmail.com>
2020-02-18 22:58:44 +0700

    เสนอกิจกรรมบำเพ็ญประโยชน์VolunteerAc_employee

--- application/views/menu/employee/menu_user_employee.php
+++ application/views/menu/employee/menu_user_employee.php
@@ -13,5 +13,8 @@
             <a href="<?php echo site_url("Notifyoffense_employee") ?>">
                 <li class="">แจ้งเหตุกระทำความผิด</li>
             </a>
+            <a href="<?php echo site_url("VolunteerAc_employee") ?>">
+                <li class="">เสนอกิจกรรมบำเพ็ญประโยชน์</li>
+            </a>
                 </ul>
     </div>
