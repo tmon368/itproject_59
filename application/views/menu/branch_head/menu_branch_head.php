783c04c8082d1d3f62afd3e48e7e97d0522d7c89
Thevenombom <venombom59114421@gmail.com>
2020-02-18 22:07:37 +0700

    VolunteerAc_branch เสนอบำเพ็ญ

--- application/views/menu/branch_head/menu_branch_head.php
+++ application/views/menu/branch_head/menu_branch_head.php
@@ -14,6 +14,9 @@
             <a href="<?php echo site_url("Notifyoffense_branch") ?>">
                 <li class="">แจ้งเหตุกระทำความผิด</li>
             </a>
+             <a href="<?php echo site_url("VolunteerAc_branch") ?>">
+                <li class="">เสนอบำเพ็ญประโยชน์</li>
+            </a>
           
         </ul>
     </div>
