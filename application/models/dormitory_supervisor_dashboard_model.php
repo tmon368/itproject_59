654b5325450ec2c3a1d6d213b23671a4b887ca0a
Boomsamui2499 <tossawat.ma@mail.wu.ac.th>
2020-02-21 14:32:23 +0700

    gg

--- application/models/dormitory_supervisor_dashboard_model.php
+++ application/models/dormitory_supervisor_dashboard_model.php
@@ -38,7 +38,9 @@ class dormitory_supervisor_dashboard_model extends CI_Model {
         }
     }
     public function getGraphData(){
-    $oc_ID = $_GET['oc_ID'];
+    // $oc_ID = $_GET['oc_ID'];
+    $oc_ID = $this->input->get('oc_ID');
+
     // $oc_ID = 8;  
     // $this->db->distinct();
     $this->db->select('dt.type_name,dm.dname as namedorm,count(ostd.S_ID) as y');   
