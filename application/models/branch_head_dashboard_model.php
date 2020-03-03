8ce5af7018d7fb74222cea10e4fec5ec8a07ce2e
Boomsamui2499 <tossawat.ma@mail.wu.ac.th>
2020-03-03 21:34:34 +0700

    จัดไปต๋อง

--- application/models/branch_head_dashboard_model.php
+++ application/models/branch_head_dashboard_model.php
@@ -236,49 +236,96 @@ public function utilstatus($statusID){
 
 
 
-     public function showAll(){
-            // $student = $this->session->userdata('student');
-            $this->db->select('*');
-            $this->db->from('place p');
-            $this->db->join('offensehead o', 'p.place_ID=o.place_ID');
-            $this->db->join('offensestd ov', 'o.oh_ID=ov.oh_ID');
-            $this->db->join('Offense os', 'o.off_ID=os.off_ID');
-            $this->db->join('Offensecate ot', 'os.oc_ID=ot.oc_ID');
-            // $this->db->where('informer', $student);
-            $query = $this->db->get();
-            $showall = array();
-            $showall = $query->result_array();
-            //var_dump($query->result());
-            //die();
+    //  public function showAll(){
+    //         // $student = $this->session->userdata('student');
+    //         $this->db->select('*');
+    //         $this->db->from('place p');
+    //         $this->db->join('offensehead o', 'p.place_ID=o.place_ID');
+    //         $this->db->join('offensestd ov', 'o.oh_ID=ov.oh_ID');
+    //         $this->db->join('Offense os', 'o.off_ID=os.off_ID');
+    //         $this->db->join('Offensecate ot', 'os.oc_ID=ot.oc_ID');
+    //         // $this->db->where('informer', $student);
+    //         $query = $this->db->get();
+    //         $showall = array();
+    //         $showall = $query->result_array();
+    //         //var_dump($query->result());
+    //         //die();
             
-        if($showall > 0){
-            return $showall;
-        }else{
-            return false;
-        }
-    }
+    //     if($showall > 0){
+    //         return $showall;
+    //     }else{
+    //         return false;
+    //     }
+    // }
     function showAlll(){
         
-        
-        //$service_ID= $this->input->post('txtdelID');
-        //$student=59111111;
-        //echo $person_ID;
-        //$student = $this->session->userdata('student');
-        $this->db->select('*');
-        $this->db->from('Service sv');
-        $this->db->join('personnel p', 'sv.person_ID=p.person_ID');
+        $i=0;
+       
+        $this->db->select('s.service_ID,s.service_name,s.proposer,s.place,s.service_date,s.start_time,s.end_time,s.received,s.number_of,s.status,s.activity_type1,s.explanation,p.person_fname,p.person_lname,p.position,p.email,p.phone1,p.phone2');
+        $this->db->from('Service s');
+        $this->db->join('personnel p', 's.person_ID=p.person_ID');
         //$this->db->where('s.S_ID', $student);
         $query = $this->db->get();
         //var_dump($query->result());
         //die;
+        // $query = $this->db->get();
+        $showall = array();
+        $showall = $query->result_array();
+        foreach($showall as $row){
+        //    $proposer= $this->selectproposer($row['proposer']);
+        // //    var_dump($proposer);
+        // //    die();
+        //    $showall[$i]['proposer_fname'] =  $proposer[0]['proposer_fname'];
+        //    $showall[$i]['proposer_lname'] =  $proposer[0]['proposer_lname'];
+        //    $showall[$i]['usertype_name'] =  $proposer[0]['usertype_name'];
+            $statusname = $this->statusservice($row['status']);
+            $showall[$i]['statusname'] = $statusname;
+            $activity_type =$this->acticity_type($row['activity_type1']);
+            $showall[$i]['activity_type_name'] = $activity_type;
+            $i+=1;
+        }
         
-        if($query->result() > 0){
-            return $query->result();
+        if($query->num_rows() > 0){
+            return $showall;
         }else{
             return false;
         }
-        
+ 
+     }
+    function statusservice($getstatus){
+        $status = ['รอบุคลากรอนุมัติ','รอเจ้าหน้าที่วินัยอนุมัติ','อนุมัติเรียบร้อย','บุคลากรไม่อนุมัติ','เจ้าหน้าที่วินัยไม่อนุมัติ',''];
+         return $status[$getstatus];
+
+
     }
+
+    function acticity_type($a_type){
+        $status = ['','บำเพ็ญประโยชน์','อบรม',''];
+         return $status[$a_type];
+
+
+    }
+    function selectproposer($usergroup){
+        // $usergroup = "jsomsri";
+         $this->db->select('std.std_fname as proposer_fname,std.std_lname as proposer_lname,ut.usertype_name as usertype_name');
+         $this->db->from('student std');
+         $this->db->join('usertype ut', 'std.usertype_ID=ut.usertype_ID');
+         $this->db->where('std.username', $usergroup);
+         $query = $this->db->get();
+         $showall = array();
+         $showall = $query->result_array();
+        
+ 
+         if($showall == null){
+         $this->db->select('p.person_fname as proposer_fname,p.person_lname as proposer_lname,ut.usertype_name as usertype_name');
+         $this->db->from('personnel p');
+         $this->db->join('usertype ut', 'p.usertype_ID=ut.usertype_ID');
+         $this->db->where('p.username', $usergroup);
+         $queryper = $this->db->get();
+         $showall = array();
+         $showall = $queryper->result_array();
+         }
+        }
     function showell(){
         
         
