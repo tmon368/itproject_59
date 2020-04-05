e423954053581eb190b2827c752f57d240836b8f
Suppakrit422 <suppakrit40@hotmail.com>
2020-04-05 22:38:31 +0700

    mainmenu

--- application/controllers/MainMenuReport.php
+++ application/controllers/MainMenuReport.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class report_test_v2 extends CI_Controller {
+class MainMenuReport extends CI_Controller {
 	function __construct(){
 		parent:: __construct();
 		$this->load->model('report2_models', 'report2_models');
@@ -15,15 +15,13 @@ class report_test_v2 extends CI_Controller {
 	}
 	
 	public function template(){
-	    $this->load->view('template/template1');
-	    $this->load->view('template/template2');
-	    $this->load->view('template/template3');
-	    $this->load->view('report_v2');
+		$this->load->view('template/template1');
+		$this->load->view('template/template2');
+		$this->load->view('menu/DormitortSupervisor/menu_user_dormsupervisor');
+		$this->load->view('template/template4');
+		$this->load->view('report/main_menu/main_menu_report_view');
 	    $this->load->view('template/template5');
 	    $this->load->view('template/template6');
-	    
-	    
-	    
 	}
 	public function chart(){
 	    $result = $this->report2_models->chart();
